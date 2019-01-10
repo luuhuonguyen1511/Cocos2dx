06088078db8306e9fcd9f0ca7aed4864a4976bc1
Uyen <luuhuonguyen1511@gmail.com>
2019-01-10 14:26:42 +0700

    +Add motivation for babytom in loanding scene

--- FistProject/Classes/LogoScene.cpp
+++ FistProject/Classes/LogoScene.cpp
@@ -19,15 +19,30 @@ bool LogoScene::init()
 	}
 
 	auto screenSize = Director::getInstance()->getVisibleSize();
-	auto logo = Sprite::create("babystar.png");
+	auto logo = Sprite::create("babytom.png");
+	logo->setScale(0.5);
 	logo->setPosition(screenSize.width / 2, screenSize.height / 2);
 	addChild(logo);
+
+	auto moveBy1 = MoveBy::create(0.5, Vec2(0, 50));
+	auto moveBy2 = MoveBy::create(1, Vec2(0, -100));
+	auto moveBy3 = MoveBy::create(0.5, Vec2(0, 50));
+	
+	auto sequence = Sequence::create(moveBy1, moveBy2, moveBy3, nullptr);
+	auto repeat = RepeatForever::create(sequence);
+
+	logo->runAction(repeat);
+
+
+	auto background = Sprite::create("background.png");
+	logo->setPosition(screenSize.width / 2, screenSize.height / 2);
+	addChild(background, -1);
 
 	auto gotoNext = CallFunc::create([]() {
 		Director::getInstance()->replaceScene(HelloWorld::createScene());
 	});
 
-	runAction(Sequence::create(DelayTime::create(3), gotoNext, nullptr));
+	runAction(Sequence::create(DelayTime::create(7), gotoNext, nullptr));
 
 	return true;
 }
